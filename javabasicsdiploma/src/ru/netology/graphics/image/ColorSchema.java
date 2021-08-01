dce5e40e219ba9a1e7d917f977552c44c137d52e
Artem Pikalov <aepikalov@gmail.com>
2021-08-01 23:46:03 +0400

    Revert "Курсовая работа по основам Java v1"
    This reverts commit 076dc9e3417e419fc197a8db59651cd440842c17.

--- javabasicsdiploma/src/ru/netology/graphics/image/ColorSchema.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.graphics.image;
-
-public class ColorSchema implements TextColorSchema {
-    @Override
-    public char convert(int color) {
-        char[] c2= {'#', '$', '@', '%', '*', '+', '-', '\''};
-        return c2[color/32];
-    }
-}
